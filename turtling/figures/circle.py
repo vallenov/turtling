d01f8f5bfe3c16561bc946f79b7425bba5156d72
vallenov <a2lenov@mail.ru>
2022-07-19 23:25:45 +0300

    Добавлена функция рисования окружностей

--- turtling/figures/circle.py
+++ turtling/figures/circle.py
@@ -2,10 +2,11 @@ import turtle
 
 
 def draw_circle():
-    turtle.tracer(0, 0)
+    turtle.speed(20)
     turtle.left(90)
-    for i in range(400):
+    for i in range(0, 400, 3):
+        turtle.circle(i)
+        turtle.right(90)
+        turtle.fd(((3.14*(i+2))-(3.14*i))//2)
         turtle.left(90)
-        turtle.circle(i, i//200)
-        turtle.fd(i)
     turtle.exitonclick()
