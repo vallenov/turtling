5f0b04f62072436247aa562ae50d18e2d1b301a0
vallenov <a2lenov@mail.ru>
2023-12-18 00:43:06 +0300

    -

--- turtling/turtling.py
+++ turtling/turtling.py
@@ -1,5 +1,7 @@
 from figures.square import draw_square
 from figures.circle import draw_circle
+from trees.base_tree import Tree, Forrest
 
 if __name__ == '__main__':
-    draw_circle()
+    forrest = Forrest()
+    forrest.grow_up()
